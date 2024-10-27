981d0b2169afd51ac6178e7bc10b0818c51a3fc8
Sean Sáez Fuller <sean.saez.f@gmail.com>
2024-10-27 15:58:45 +0100

    Padding bottom added in the page

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Header />
-    <main class="pt-20 px-4 sm:px-6 lg:px-8 max-w-screen-xl mx-auto">
+    <main class="py-20 px-4 sm:px-6 lg:px-8 max-w-screen-xl mx-auto">
       <h1 class="text-2xl mb-6 text-[#323349]">YACHTS FOR SALE - 340</h1>
       <NuxtPage />
     </main>
